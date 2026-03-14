b25039bd2423167928c2bf1b1c22ce48792fc4ae
Lucas Lemos <lucaslemoss608@gmail.com>
2026-03-14 14:10:52 -0300

    fazendo

---
+++ exercicio21.php
@@ -0,0 +1,45 @@
+<!-- 21. Escrever um algoritmo que leia os dados de “N” pessoas (nome, sexo, idade e saúde) e informe se está apta ou não
+para cumprir o serviço militar obrigatório. Informe os totais. -->
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Questão 21</title>
+</head>
+<body>
+<form method="post">
+
+<input type="text" name="nome" namespace="Digite seu Nome completo: ">
+<input type="number" name="idade" namespace="Digite o número da sua idade: ">
+
+
+<label for="sexo">Selecione seu sexo:  </label>
+    <select name="sexo">
+        <option value="genero1">Masculino</option>
+        <option value="genero2">Feminino</option>
+    </select>
+
+<label for="saude">Selecione sua saúde:  </label>
+    <select name="saude">
+        <option value="saude1">saúde Boa</option>
+        <option value="saude2">saúde Ruim</option>
+    </select>
+
+
+
+</form>
+
+</body>
+</html>
+
+
+
+<?php
+
+
+
+
+
+
+?>
